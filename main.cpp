6fa21464973dca066e138bc7caa2830b9600b7d9
zbw0430 <691189575@qq.com>
2022-05-09 21:35:38 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  2022.5.6
+//
+//  Created by 暖暖 on 2022/5/6.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int a = 0;
+    cin >> a;
+    cout << a << endl;
+    return 0;
+}
